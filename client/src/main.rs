991b0f7bffa2b37be9f35d73976f707b97cee20a
Seba G <sebag.git@gmail.com>
2024-01-10 21:50:32 +0100

    feat(client) read tun packets

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,41 @@
+use etherparse::SlicedPacket;
+use riptun::Tun;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let tun = match Tun::new("vpn%d", 1) {
+        Ok(tun) => tun,
+        Err(err) => {
+            println!("[ERROR] => {}", err);
+            return;
+       }
+    };
+
+    // Lets make sure we print the real name of our new TUN device.
+    println!("[INFO] => Created TUN '{}'!", tun.name());
+
+    // Create a buffer to read packets into, and setup the queue to receive from.
+    let mut buffer: [u8; 1500] = [0x00; 1500];
+    let queue = 0;
+
+    // Loop forever reading packets off the queue.
+    loop {
+        // Receive the next packet from the specified queue.
+        let read = match tun.recv_via(queue, &mut buffer) {
+            Ok(read) => read,
+            Err(err) => {
+                println!("[ERROR] => {}", err);
+                return;
+            }
+        };
+
+        let packet = SlicedPacket::from_ip(&buffer[..read]).unwrap();
+
+        // Print out the amount of data received and the bytes read off the queue.
+        println!(
+            "[INFO] => Received packet data ({}B): {:?}",
+            read,
+            packet
+        );
+    }
 }
